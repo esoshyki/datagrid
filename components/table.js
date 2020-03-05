30b295793d98c5a123088aa9f14d404b07b17f6a
esoshyki <esoshyki@yandex.by>
2020-03-05 21:39:53 +0300

    feat: add material-table component

--- components/table.js
+++ components/table.js
@@ -1,28 +1,68 @@
 import React from 'react';
+import MaterialTable, { Column } from 'material-table';
 
 const DataTable = ({users}) => {
+
+  const [state, setState ] = React.useState({
+    columns: [
+      { title:  'Name', field: 'name'},
+      { title:  'Email', field: 'email'},
+      { title:  'nickName', field: 'nickName'},
+      { title:  'age', field: 'age'},
+    ],
+    data: users,
+    selectedRow: null
+  })
   return users ? (
-    <div>
-      <table>
-      <tr>
-        <th>Имя</th>
-        <th>Почта</th>
-        <th>Ник</th>
-        <th>Возвраст</th>
-      </tr>
-      {users.map((element, index) => {
-          const { name, email, nickName, age } = element
-          return (
-            <tr key={index}>
-              <td>{name}</td>
-              <td>{email}</td>
-              <td>{nickName}</td>
-              <td>{age}</td>
-            </tr>
-          )
-      })}
-      </table>
-    </div>
+    <MaterialTable
+      title="Users"
+      columns={state.columns}
+      data={state.data}
+      options={{
+        rowStyle: {
+          "&:hover" : {backgroundColor: 'black'}
+        },
+        // pageSize: users.length,
+        exportFileName: 'users.cvs'
+      }}
+      editable={{
+        onRowAdd: newData => 
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              setState(prevState => {
+                const data = [...prevState.data];
+                data.push(newData);
+                return {...prevState, data}
+              })
+            }, 600)
+          }),
+          onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              if (oldData) {
+                setState(prevState => {
+                  const data = [...prevState.data];
+                  data[data.indexOf(oldData)] = newData;
+                  return { ...prevState, data };
+                });
+              }
+            }, 600);
+          }),
+          onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              setState(prevState => {
+                const data = [...prevState.data];
+                data.splice(data.indexOf(oldData), 1);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      }} />
+    
   ) : <div>Loading</div>
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import { connect } from "react-redux"
 import DataTable from '../components/table'
 import personsData from '../data/data.json';
+import Head from 'next/head';
+import '../style/style.sass'
 
 const Index = ({count, users, dispatch}) => {
 
   return users ? (
     <div>
-      {count}
-      <button onClick={() => dispatch({type: "ADD"})}>add</button>
+      <Head>
+      <meta 
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+        key="viewport"/>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      </Head>
+      <div>
       <DataTable users={users}/>
+      </div>
     </div>
   ) : <div>Loading...</div>
 }
