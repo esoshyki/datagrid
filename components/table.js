323555731b4c02f62d1f7c3fd43f6609dfe9c07f
esoshyki <esoshyki@yandex.by>
2020-03-09 17:07:47 +0300

    feat: realize stycky header

--- components/table.js
+++ components/table.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import MaterialTable, { Column } from 'material-table';
+import MaterialTable, { MTableBody } from 'material-table';
+import { FixedSizeList as List} from 'react-window';
+import AutoSizer from 'react-virtualized-auto-sizer';
 
 const DataTable = ({users}) => {
 
-  const filtering = () => {
-    console.log('filtering')
-  }
   const [selectedRow, setSelectedRow ] = useState([])
   const [state, setState ] = React.useState({
     columns: [
-      { title: 'Name', field: 'name', customFilterAndSearch: filtering, hidden: false},
+      { title: 'Name', field: 'name', hidden: false},
       { title: 'Email', field: 'email',  hidden: false},
       { title: 'nickName', field: 'nickName',  hidden: false, type: 'string'},
       { title: 'age', field: 'age', hidden: false, type: 'numeric'},
@@ -28,82 +27,98 @@ const DataTable = ({users}) => {
     console.log(selectedRow)
   })
 
+  const handleScroll = (event) => {
+    console.log(event)
+  }
+
   return users ? (
-    <MaterialTable
-      title="Users"
-      columns={state.columns}
-      data={state.data}
-      options={{
-        rowStyle: rowData => ({
-            backgroundColor:  (selectedRow.length > 0 && selectedRow.includes(rowData.tableData.id)) ? '#EEE' : '#FFF'
-          }),
-        exportButton: true,
-        exportFileName: 'users',
-        filtering: true,
-        selection: true,
-        columnsButton: true
-      }}
-      localization={{
-        header: {
-          actions: 'Actions'
-      },
-      }}
-      onSelectionChange= {(rows) => setSelectedRow(rows.map(el => el.tableData.id))}
-      actions={[
-        {
-          tooltip: 'Remove All Selected Users',
-          icon: 'delete',
-          onClick: (evt, data) => {
-            setState(prevData => {
-            const newData = [...prevData.data].filter((row, idx) => {
-              const indexes = data.map(el => el.tableData.id);
-              return !indexes.includes(idx)
-            })
-            return {...prevData, data: newData}
-          });
-          setSelectedRow([])
-          }
-        }
-      ]}
-      editable={{
-        onRowAdd: newData => 
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return {...prevState, data}
+    <div onScroll={handleScroll}>
+      <MaterialTable
+        title="Users"
+        columns={state.columns}
+        data={state.data}
+        options={{
+          rowStyle: rowData => ({
+              backgroundColor:  (selectedRow.length > 0 && selectedRow.includes(rowData.tableData.id)) ? '#EEE' : '#FFF'
+            }),
+          exportButton: true,
+          exportFileName: 'users',
+          filtering: true,
+          selection: true,
+          columnsButton: true,
+          maxBodyHeight: 400,
+          paginationType: 'stepped',
+          initialPage: 0,
+          pageSize: 10,
+          headerStyle: { position: 'sticky', top: 0 },
+        }}
+        localization={{
+          header: {
+            actions: 'Actions'
+        },
+        }}
+        onSelectionChange= {(rows) => setSelectedRow(rows.map(el => el.tableData.id))}
+
+        actions={[
+          {
+            tooltip: 'Remove All Selected Users',
+            icon: 'delete',
+            onClick: (evt, data) => {
+              setState(prevData => {
+              const newData = [...prevData.data].filter((row, idx) => {
+                const indexes = data.map(el => el.tableData.id);
+                return !indexes.includes(idx)
               })
-            }, 600)
-          }),
-          onRowUpdate: (newData, oldData) =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
+              return {...prevData, data: newData}
+            });
+            setSelectedRow([])
+            }
+          }
+        ]}
+
+        editable={{
+
+          onRowAdd: newData => 
+            new Promise(resolve => {
+              setTimeout(() => {
+                resolve();
                 setState(prevState => {
                   const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
+                  data.push(newData);
+                  return {...prevState, data}
+                })
+              }, 600)
+            }),
+
+            onRowUpdate: (newData, oldData) =>
+            new Promise(resolve => {
+              setTimeout(() => {
+                resolve();
+                if (oldData) {
+                  setState(prevState => {
+                    const data = [...prevState.data];
+                    data[data.indexOf(oldData)] = newData;
+                    return { ...prevState, data };
+                  });
+                }
+              }, 600);
+            }),
+
+            onRowDelete: oldData =>
+            new Promise(resolve => {
+              setTimeout(() => {
+                resolve();
+                setState(prevState => {
+                  const data = [...prevState.data];
+                  data.splice(data.indexOf(oldData), 1);
                   return { ...prevState, data };
                 });
-              }
-            }, 600);
-          }),
-          onRowDelete: oldData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
-       />
-    
+              }, 600);
+            }),
+
+        }}
+         />
+   </div>
   ) : <div>Loading</div>
 }
 
