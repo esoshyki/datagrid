9abbcb983af1fba3bcde7f8deacc0e86a7bbd2da
esoshyki <esoshyki@yandex.by>
2020-03-09 11:41:37 +0300

    feat: add several rows pick and actions on them

--- components/table.js
+++ components/table.js
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MaterialTable, { Column } from 'material-table';
 
 const DataTable = ({users}) => {
 
+  const filtering = () => {
+    console.log('filtering')
+  }
+  const [selectedRow, setSelectedRow ] = useState([])
   const [state, setState ] = React.useState({
     columns: [
-      { title: 'Name', field: 'name'},
+      { title: 'Name', field: 'name', customFilterAndSearch: filtering},
       { title: 'Email', field: 'email'},
       { title: 'nickName', field: 'nickName'},
       { title: 'age', field: 'age'},
-      { title: 'status', field: 'status'},
+      { title: 'status', field: 'status',
+        lookup: 
+        { production: 'production',
+        support: 'support', design: 'design' }},
       { title: 'is married', field: 'married'},
       { title: 'exam date', field: 'exam'}
     ],
     data: users,
     selectedRow: null
   })
+
+  useEffect(() => {
+    console.log(selectedRow)
+  })
+
   return users ? (
     <MaterialTable
       title="Users"
       columns={state.columns}
       data={state.data}
       options={{
-        rowStyle: {
-          "&:hover" : {backgroundColor: 'black'}
-        },
-        // pageSize: users.length,
+        rowStyle: rowData => ({
+            backgroundColor:  (selectedRow.length > 0 && selectedRow.includes(rowData.tableData.id)) ? '#EEE' : '#FFF'
+          }),
         exportButton: true,
-        exportFileName: 'users'
+        exportFileName: 'users',
+        filtering: true,
+        selection: true,
       }}
+      onSelectionChange= {(rows) => setSelectedRow(rows.map(el => el.tableData.id))}
+      actions={[
+        {
+          tooltip: 'Remove All Selected Users',
+          icon: 'delete',
+          onClick: (evt, data) => {
+            setState(prevData => {
+            const newData = [...prevData.data].filter((row, idx) => {
+              const indexes = data.map(el => el.tableData.id);
+              return !indexes.includes(idx)
+            })
+            return {...prevData, data: newData}
+          });
+          setSelectedRow([])
+          }
+        }
+      ]}
       editable={{
         onRowAdd: newData => 
           new Promise(resolve => {
@@ -65,15 +95,7 @@ const DataTable = ({users}) => {
               });
             }, 600);
           }),
-          onRowClick: (e) => {
-            console.log(e)
-          }
       }}
-      onRowClick={(event, row) => {
-        console.log(event)
-        console.log(row)
-      }}
-
        />
     
   ) : <div>Loading</div>
